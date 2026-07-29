036abd3152b97c2c48b16ab4bb044d5d81b5411c
Arun Tester <arun_ap@example.com>
2026-07-30 01:07:54 +0530

    fix: explicit tracing for pdfkit fonts on Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  serverExternalPackages: ['pdfmake', 'pdfkit'],
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/**/*': ['./node_modules/pdfkit/js/data/**/*']
+    }
+  }
 };
 
 export default nextConfig;
